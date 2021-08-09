4b5d072f1bf5e7140d24c65bafeaceb3cc86b561
Adloya <34348778+Adloya@users.noreply.github.com>
2021-08-09 21:47:01 +0200

    Upgrade to DJS 13 [2nd Upload -- Lists & Events]
    -status.json
    -language.json
    -emojis.json
    -colors.json
    -guildMemberAdd.js
    -ready.js
    -messageCreate.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,15 +1,13 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+// const client = new Discord.Client();
+const client = new Discord.Client({ intents: 32767 });
 const statusFile = require("../../lists/status.json")
 
 
 client.commands = new Discord.Collection();
 
 module.exports = (client, message) => {
-    
-    console.log("=================================================")
-    console.log("['DiscordJS : '12.5.3']");
-    console.log(`=================================================\n[Toaster] : Online! (logged in as ${client.user.tag})\n=================================================`);
+    console.log(`[Toaster] : Online! (logged in as ${client.user.tag})\n=================================================`);
 
     let statuts = statusFile.stats;
     setInterval(function() {
--- src/events/guilds/guildMemberAdd.js
+++ src/events/guilds/guildMemberAdd.js
@@ -1,15 +1,10 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+// const client = new Discord.Client();
+const client = new Discord.Client({ intents: 32767 });
 const db = require("../../db.json");
 
 client.commands = new Discord.Collection();
 
 module.exports = async (client, member) => {
-    if(db[member.guild.id]["anti-join"] === "on"){
-        console.log(db[guild.id]["anti-join"])
-        member.kick("Antijoin was enabled");
-        console.log("ah!")
-    }else{
-        console.log("ah?")
-    }
+    // rien a voir pour l'instant :(
 }
